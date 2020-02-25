0c7d7abbc109666f030c1193fe1449935258be1d
vivekmprac <59341220+vivekmprac@users.noreply.github.com>
2020-02-26 00:41:26 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java welcome vivek");  
+     System.out.println("Hello Java welcome vivek mahendra 111");  
     }  
 }  
